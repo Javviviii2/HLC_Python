0efac8328cbd928e0d429c92b5fc16a3efe663a9
Javi <jrofe96@gmail.com>
2024-01-31 08:38:27 +0100

    iniciando repositorio

---
+++ practicas-retos/practicas/31-01-24/ej1.py
@@ -0,0 +1,3 @@
+# Ejercicio 1: Introducción a las Clases
+# Tarea: Crea una clase `Coche` que tenga dos atributos: `marca` y `modelo`. Incluye un método `mostrar_info()` que imprima "Marca: [marca], Modelo: [modelo]".
+# Bonus: Añade un método `cambiar_modelo()` que permita modificar el atributo `modelo`.
